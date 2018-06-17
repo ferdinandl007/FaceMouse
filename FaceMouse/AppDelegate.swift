3c5f876ce8885f27208a1ba39d0b1679e0163b8f
ferdinandl007 <ferdinandloesch@me.com>
2018-06-17 12:06:38 +0200

    Initial Commit

---
+++ FaceMouse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FaceMouse
+//
+//  Created by Ferdinand Lösch on 17.06.18.
+//  Copyright © 2018 Ferdinand Lösch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
